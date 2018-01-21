8ab21b6652be58414833ae62cdf422460050a12f
aburd <burdick.aaron@gmail.com>
2018-01-21 17:18:37 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    const indexFile = path.join(__dirname, './index.html')
+    res.sendFile(indexFile)
+})
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('Got a post')
+})
+app.listen(3000, () => console.log('ポート3000にアプリを開いてみてください!'))
+
+
